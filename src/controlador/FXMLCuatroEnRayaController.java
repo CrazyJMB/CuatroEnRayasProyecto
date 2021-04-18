062af22aad8a66bb37cff26bfaaccfbaa44e4fe1
CrazyJMB <crazyjmbemail@gmail.com>
2021-04-18 07:52:57 +0200

    Vista crear usuario

--- src/controlador/FXMLCuatroEnRayaController.java
+++ src/controlador/FXMLCuatroEnRayaController.java
@@ -1,15 +1,15 @@
-package controlador;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
@@ -24,6 +24,10 @@ public class FXMLCuatroEnRayaController implements Initializable {
     private TextField UserName;
     @FXML
     private TextField UserPassword;
+    @FXML
+    private Label CrearNuevaCuentaButton;
+    @FXML
+    private Label VerRankingButton;
 
     /**
      * Initializes the controller class.
@@ -32,6 +36,10 @@ public class FXMLCuatroEnRayaController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void LogInAccount(MouseEvent event) {
+    }
 
     @FXML
     private void CreateNewAccount(MouseEvent event) {
