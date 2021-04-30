2311eb1f3e0c06fb9683614eea235a0af5173c68
CrazyJMB <crazyjmbemail@gmail.com>
2021-04-30 23:09:11 +0200

    Primera entrega

--- src/aplicacion/aplicacion.java
+++ src/aplicacion/aplicacion.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -28,6 +29,7 @@ public class aplicacion extends Application {
         
         stage.setScene(scene);
         stage.setResizable(false);
+        stage.getIcons().add(new Image("/img/Logo16x16.png", false));
         stage.setTitle("Inicio de sesion");
         stage.show();
     }
